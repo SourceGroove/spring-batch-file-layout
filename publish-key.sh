b1a3f24124942b053a0ceeb950411a0014eead3f
Seth Jordan <sjordan@madenasolutions.com>
2020-01-07 11:46:49 -0500

    Pre release

--- publish-key.sh
+++ publish-key.sh
@@ -1,1 +1,3 @@
-gpg --send-keys –keyserver pool.sks-keyservers.net –send-key "$KEY_ID"
+gpg --full-generate-key
+
+gpg --keyserver pool.sks-keyservers.net --send-key E5945D71CB7818905BC298602FCF5188F3F73042
